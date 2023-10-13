45378fc0b2c9cdd803b5fff16af5f191ddbce259
Matheus Barros Marques Guimaraes <matheus.marques@g.globo>
2023-10-13 12:07:42 -0300

    update 8

--- src/app/benefits-page/benefits-page.component.ts
+++ src/app/benefits-page/benefits-page.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-benefits-page',
   templateUrl: './benefits-page.component.html',
   styleUrls: ['./benefits-page.component.scss']
 })
-export class BenefitsPageComponent implements OnInit {
+export class BenefitsPageComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('carousel', { static: true }) carousel!: ElementRef;
+
+  constructor(
+    private renderer: Renderer2
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    const carouselElement = this.renderer.selectRootElement('#carousel');
+    this.renderer.setProperty(carouselElement, 'data-interval', 0);
+
+    // this.renderer.setProperty(this.carousel.nativeElement, 'data-interval', 0);
   }
 
 }
